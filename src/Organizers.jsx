ba837a534f79cec595ec423d7f10130b6c855aa4
Shreya-1305 <paintershreya@gmail.com>
2024-09-28 19:27:21 +0530

    organizers name added

--- src/Organizers.jsx
+++ src/Organizers.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './Organizers.css'; // Assume you'll add some CSS for styling
+import React from "react";
+import "./Organizers.css"; // Assume you'll add some CSS for styling
 
 const Organizers = () => {
   return (
@@ -7,7 +7,8 @@ const Organizers = () => {
       {/* Organizers Section */}
       <h1 style={styles.heading}>
         Organizing <span style={styles.highlight}>Committee</span>
-      </h1>      <div className="organizer-group">
+      </h1>{" "}
+      <div className="organizer-group">
         <h3>PATRON</h3>
         <p>Dr. Nilay N Bhuptani</p>
       </div>
@@ -28,22 +29,29 @@ const Organizers = () => {
         <p>Prof. Swati Patel</p>
         <p>Prof. Nirjari Desai</p>
       </div>
+      <div className="organizer-group">
+        <h3>ORGANIZING COMMITTEE</h3>
+        <p>Dr. Tejas Modi</p>
+        <p>Dr. Diya Vadhwani</p>
+        <p>Dr. Maitri Vaghela</p>
+        <p>Dr. Ritu Padhiar</p>
+      </div>
     </div>
   );
 };
 const styles = {
-    heading: {
-      fontSize: '24px', // Adjusted font size
-      fontWeight: '700',
-      color: '#003366',
-      marginBottom: '10px',
-      borderBottom: '2px solid #003366',
-      display: 'inline-block',
-      paddingBottom: '5px',
-    },
-    highlight: {
-      color: '#ffb347',
-    },
-  };
-  
+  heading: {
+    fontSize: "24px", // Adjusted font size
+    fontWeight: "700",
+    color: "#003366",
+    marginBottom: "10px",
+    borderBottom: "2px solid #003366",
+    display: "inline-block",
+    paddingBottom: "5px",
+  },
+  highlight: {
+    color: "#ffb347",
+  },
+};
+
 export default Organizers;
